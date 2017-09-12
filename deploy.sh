09c1f29f27af9916b72054e9d4198bc1b6b710fb
prashant <prashant.joshi@systematixindia.com>
2017-09-12 11:58:55 +0530

    ji

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # A simple script
 
-aws s3api create-bucket --bucket my-bucket911 --region ap-south-1
+# aws s3api create-bucket --bucket my-bucket911 --region ap-south-1
+  aws s3api create-bucket --bucket my-bucket1011 --region ap-south-1 --create-bucket-configuration LocationConstraint=ap-south-1
